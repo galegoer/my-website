16253223c60fc1eb4962c487624546f88482a9d8
galegoer <eric.m.galego@gmail.com>
2020-07-16 18:59:24 -0400

    Added random 2010s music YouTube player
    -Untested on https (live server doesn't currently work)

---
+++ vidMain.js
@@ -0,0 +1,46 @@
+const KEY = "AIzaSyBkBVdemI8E-QQzmawYCzJe3UpzM3GaoPc";
+
+function provideVid(vidNum, counter, url, changeVideo) {
+
+    fetch(url)
+    .then(response => response.json())
+    .then(function(data) {
+        console.log(data);
+        if(vidNum == -1) {
+            //not randomized yet
+            vidNum = Math.floor(Math.random() * data.pageInfo.totalResults);
+        }
+        resultsPerPage = data.pageInfo.resultsPerPage;
+        for(var vid=0; vid < resultsPerPage; vid++) {
+            if(counter == vidNum) {
+                var vidId = data.items[vid]["contentDetails"]["videoId"];
+                console.log(vidId);
+                return changeVideo(vidId);
+            }
+            counter++;
+        }       
+        nextToken = data.nextPageToken;
+        url = "https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails&playlistId=PLArQ6Xij15ikMnefk5BL5PwdTLfqmtMNz&pageToken="+nextToken+"&maxResults=50&key="+KEY;
+        return provideVid(vidNum, counter, url, changeVideo); 
+    })
+    .catch(e => {
+        console.log("Error: " + e);
+    })
+} 
+
+function changeVideo(vidId) {
+    // const vidId = provideVid(-1, 0, url);
+    // let vidId = provideVid(-1, 0, url);
+    console.log('VIDID : '+ vidId);
+    let vidFrame = document.getElementById('videoFrame');
+    vidFrame.src = "https://www.youtube.com/embed/"+vidId;
+}
+
+window.onload = function() {
+    var url="https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails&playlistId=PLArQ6Xij15ikMnefk5BL5PwdTLfqmtMNz&maxResults=50&key="+KEY;
+    document.getElementById('randomizeVid').onclick = function() {
+        provideVid(-1, 0, url, function(vidId) {
+            changeVideo(vidId);
+        });
+    }
+}
